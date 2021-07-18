550eb41a8067f62a7c626216e3eda5cfd02c12b3
Zmey14 <mishkaneshishka@gmail.com>
2021-07-18 17:58:17 +0300

    error handling

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+from .pages.main_page import MainPage
 
-
-def go_to_link_hage(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_link_hage(browser)    
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
